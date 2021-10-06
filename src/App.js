f653728a5e10ca19aa36df3ccc1aa270f0b8ee9d
Sandon <sandonnlai@gmail.com>
2021-10-06 15:29:01 +1100

    added supercluster markers, converted markers into buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Map from './components/Map'
 import './App.css';
-import * as cafeData from "./data/cafe-restaurant-bistro-seats-2019.json";
 
 function App() {
   return (
--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import ReactMapGL, { Marker } from "react-map-gl";
+import useSupercluster from "use-supercluster";
 import "mapbox-gl/dist/mapbox-gl.css";
 import * as cafeData from "../../data/cafe-restuarants-2019.json";
 
@@ -13,6 +14,38 @@ const Map = () => {
   const accessToken =
     "pk.eyJ1Ijoic2FuZG9ubCIsImEiOiJja3QzbnRsazcwOWoyMndudW94N2M5Y3gyIn0.W4x7VhJckEqamtkQE-e9yA";
 
+  // Map Reference
+  const mapRef = useRef();
+
+  // Cluster points
+  const points = cafeData.features.map((cafe) => ({
+    type: "Cafe",
+    properties: {
+      cluster: false,
+      ID: cafe.ID,
+      name: cafe["Trading name"],
+      seatingType: cafe["Seating type"],
+      address: cafe["Street address"],
+    },
+    geometry: {
+      type: "Point",
+      coordinates: [cafe.longitude, cafe.latitude],
+    },
+  }));
+
+  // Get map bounds
+  const bounds = mapRef.current
+    ? mapRef.current.getMap().getBounds().toArray().flat()
+    : null;
+
+  // Get Clusters
+  const { clusters } = useSupercluster({
+    points,
+    zoom: viewport.zoom,
+    bounds,
+    options: { radius: 75, maxZoom: 20 },
+  });
+
   return (
     <div>
       <ReactMapGL
@@ -22,16 +55,45 @@ const Map = () => {
         height="100vh"
         mapboxApiAccessToken={accessToken}
         mapStyle="mapbox://styles/sandonl/cku84fkct0b3w18pdckgthaua/draft"
+        ref={mapRef}
       >
-        {cafeData.features.map((cafe) => (
-          <Marker
-            key={cafe.ID}
-            latitude={cafe.latitude}
-            longitude={cafe.longitude}
-          >
-            <div>TEST</div>
-          </Marker>
-        ))}
+        {clusters.map((cluster) => {
+          const [longitude, latitude] = cluster.geometry.coordinates;
+          const { cluster: isCluster, point_count: pointCount } =
+            cluster.properties;
+
+          if (isCluster) {
+            return (
+              <Marker
+                key={cluster.id}
+                latitude={latitude}
+                longitude={longitude}
+              >
+                <div
+                  className="cluster-marker"
+                  style={{
+                    width: `${10 + (pointCount / points.length) * 20}`,
+                    height: `${10 + (pointCount / points.length) * 20}`,
+                  }}
+                >
+                  {pointCount}
+                </div>
+              </Marker>
+            );
+          }
+
+          return (
+            <Marker
+              key={cluster.properties.ID}
+              latitude={latitude}
+              longitude={longitude}
+            >
+              <div className="marker">
+                <button className="marker-button">O</button>
+              </div>
+            </Marker>
+          );
+        })}
       </ReactMapGL>
     </div>
   );
